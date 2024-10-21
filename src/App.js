c9fd7c4ed0c2451b3a4add883c5921b3b09d8f2a
piotr <p@wp.pl>
2024-10-21 19:59:02 +0200

    working copy

--- src/App.js
+++ src/App.js
@@ -1,21 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useCallback } from 'react';
 import { useSession, useSupabaseClient, useSessionContext } from '@supabase/auth-helpers-react';
 import DateTimePicker from 'react-datetime-picker';
-import { useState } from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+
+const localizer = momentLocalizer(moment);
 
 function App() {
-  const [ start, setStart ] = useState(new Date());
-  const [ end, setEnd ] = useState(new Date());
-  const [ eventName, setEventName ] = useState("");
-  const [ eventDescription, setEventDescription ] = useState("");
+  const [events, setEvents] = useState([]);
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [selectedTime, setSelectedTime] = useState("");
+  const [selectedService, setSelectedService] = useState("");
+  const [patientName, setPatientName] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [currentRange, setCurrentRange] = useState({ start: new Date(), end: new Date() });
 
-  const session = useSession(); // tokens, when session exists we have a user
-  const supabase = useSupabaseClient(); // talk to supabase!
+  const session = useSession();
+  const supabase = useSupabaseClient();
   const { isLoading } = useSessionContext();
-  
-  if(isLoading) {
-    return <></>
+
+  const fetchCalendarEvents = useCallback(async (startDate, endDate) => {
+    const start = startDate.toISOString();
+    const end = endDate.toISOString();
+
+    const response = await fetch(`https://www.googleapis.com/calendar/v3/calendars/primary/events?timeMin=${start}&timeMax=${end}`, {
+      headers: {
+        'Authorization': 'Bearer ' + session.provider_token
+      }
+    });
+
+    const data = await response.json();
+    if (data.items) {
+      const formattedEvents = data.items.map(event => ({
+        id: event.id,
+        title: event.summary,
+        start: new Date(event.start.dateTime),
+        end: new Date(event.end.dateTime),
+      }));
+      setEvents(formattedEvents);
+    } else {
+      setEvents([]);
+    }
+  }, [session]);
+
+  useEffect(() => {
+    if (session) {
+      fetchCalendarEvents(currentRange.start, currentRange.end);
+    }
+  }, [session, currentRange]);
+
+  if (isLoading) {
+    return <></>;
   }
 
   async function googleSignIn() {
@@ -25,9 +60,9 @@ function App() {
         scopes: 'https://www.googleapis.com/auth/calendar'
       }
     });
-    if(error) {
-      alert("Error logging in to Google provider with Supabase");
-      console.log(error);
+    if (error) {
+      alert("Błąd logowania do Google przez Supabase");
+      console.error(error);
     }
   }
 
@@ -35,63 +70,143 @@ function App() {
     await supabase.auth.signOut();
   }
 
+  function handleRangeChange(range) {
+    if (range.start && range.end) {
+      setCurrentRange({ start: range.start, end: range.end });
+    }
+  }
+
   async function createCalendarEvent() {
-    console.log("Creating calendar event");
+    if (!patientName || !phoneNumber || !selectedService || !selectedDate || !selectedTime) {
+      alert("Proszę wypełnić wszystkie pola.");
+      return;
+    }
+
+    const startDateTime = new Date(selectedDate);
+    startDateTime.setHours(parseInt(selectedTime.split(":")[0], 10));
+    startDateTime.setMinutes(parseInt(selectedTime.split(":")[1], 10));
+
     const event = {
-      'summary': eventName,
-      'description': eventDescription,
+      'summary': selectedService,
+      'description': `Pacjent: ${patientName}, Telefon: ${phoneNumber}`,
       'start': {
-        'dateTime': start.toISOString(), // Date.toISOString() ->
-        'timeZone': Intl.DateTimeFormat().resolvedOptions().timeZone // America/Los_Angeles
+        'dateTime': startDateTime.toISOString(),
+        'timeZone': Intl.DateTimeFormat().resolvedOptions().timeZone
       },
       'end': {
-        'dateTime': end.toISOString(), // Date.toISOString() ->
-        'timeZone': Intl.DateTimeFormat().resolvedOptions().timeZone // America/Los_Angeles
+        'dateTime': new Date(startDateTime.getTime() + 60 * 60 * 1000).toISOString(),
+        'timeZone': Intl.DateTimeFormat().resolvedOptions().timeZone
       }
-    }
+    };
+
     await fetch("https://www.googleapis.com/calendar/v3/calendars/primary/events", {
       method: "POST",
       headers: {
-        'Authorization':'Bearer ' + session.provider_token // Access token for google
+        'Authorization': 'Bearer ' + session.provider_token,
+        'Content-Type': 'application/json'
       },
       body: JSON.stringify(event)
-    }).then((data) => {
-      return data.json();
-    }).then((data) => {
-      console.log(data);
-      alert("Event created, check your Google Calendar!");
-    });
+    }).then((response) => response.json())
+      .then((data) => {
+        alert("Wydarzenie utworzone, sprawdź swój Kalendarz Google!");
+        fetchCalendarEvents(currentRange.start, currentRange.end);
+      })
+      .catch((error) => {
+        alert("Błąd przy tworzeniu wydarzenia");
+        console.error(error);
+      });
   }
 
-  console.log(session);
-  console.log(start);
-  console.log(eventName);
-  console.log(eventDescription);
+  const availableTimes = [
+    "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00"
+  ];
+
+  const services = [
+    { name: "Konsultacja indywidualna", price: "200 zł", duration: "50 min" },
+    { name: "Terapia par", price: "250 zł", duration: "80 min" },
+    { name: "Coaching", price: "300 zł", duration: "60 min" },
+  ];
+
   return (
-    <div className="App">
-      <div style={{ width: "800px", margin: "20px auto", padding: "10px", borderRadius: "8px", boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)", backgroundColor: "#f9f9f9" }}>
-        {session ?
-          <>
-            <h2 style={{ color: "#333", textAlign: "center" }}>Hey there {session.user.email}</h2>
-            <p style={{ fontWeight: "bold" }}>Start of your event</p>
-            <input type="datetime-local" onChange={(e) => setStart(new Date(e.target.value))} value={start.toISOString().slice(0, 16)} style={{ width: "100%", height: "50px" }} />
-            <p style={{ fontWeight: "bold" }}>End of your event</p>
-            <input type="datetime-local" onChange={(e) => setEnd(new Date(e.target.value))} value={end.toISOString().slice(0, 16)} style={{ width: "100%", height: "50px" }} />
-            <p style={{ fontWeight: "bold" }}>Event name</p>
-            <input type="text" onChange={(e) => setEventName(e.target.value)} style={{ width: "100%", padding: "10px", borderRadius: "4px", border: "1px solid #ccc" }} />
-            <p style={{ fontWeight: "bold" }}>Event description</p>
-            <input type="text" onChange={(e) => setEventDescription(e.target.value)} style={{ width: "100%", padding: "10px", borderRadius: "4px", border: "1px solid #ccc" }} />
-            <hr />
-            <button onClick={() => createCalendarEvent()} style={{ width: "100%", padding: "10px", borderRadius: "4px", backgroundColor: "#4CAF50", color: "white", border: "none", cursor: "pointer" }}>Create Calendar Event</button>
-            <p></p>
-            <button onClick={() => signOut()} style={{ width: "100%", padding: "10px", borderRadius: "4px", backgroundColor: "#f44336", color: "white", border: "none", cursor: "pointer" }}>Sign Out</button>
-          </>
-          :
-          <>
-            <button onClick={() => googleSignIn()} style={{ width: "100%", padding: "10px", borderRadius: "4px", backgroundColor: "#4285F4", color: "white", border: "none", cursor: "pointer" }}>Sign In With Google</button>
-          </>
-        }
-      </div>
+    <div style={{ maxWidth: '800px', margin: '20px auto', padding: '20px', borderRadius: '8px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', backgroundColor: '#f9f9f9' }}>
+      {session ? (
+        <>
+          <div style={{ textAlign: 'center', marginBottom: '20px' }}>
+            <h2 style={{ fontSize: '24px', fontWeight: 'bold' }}>Witaj, {session.user.email}</h2>
+          </div>
+          
+          <div style={{ marginBottom: '20px' }}>
+            <h3 style={{ fontSize: '20px', fontWeight: 'bold', textAlign: 'center' }}>Cennik usług</h3>
+            <div style={{ display: 'grid', gap: '10px' }}>
+              {services.map((service, index) => (
+                <div key={index} style={{ border: selectedService === service.name ? '2px solid #007BFF' : '1px solid #ddd', borderRadius: '8px', padding: '10px' }}>
+                  <h4 style={{ fontSize: '18px', fontWeight: 'bold' }}>{service.name}</h4>
+                  <p style={{ fontSize: '16px' }}>{service.price}</p>
+                  <p style={{ fontSize: '14px', color: '#555' }}>Czas trwania: {service.duration}</p>
+                  <button 
+                    onClick={() => setSelectedService(service.name)}
+                    style={{ width: '100%', padding: '10px', marginTop: '10px', backgroundColor: selectedService === service.name ? '#007BFF' : '#f0f0f0', color: selectedService === service.name ? '#fff' : '#333', border: 'none', borderRadius: '4px', cursor: 'pointer' }}
+                  >
+                    {selectedService === service.name ? "Wybrano" : "Wybierz"}
+                  </button>
+                </div>
+              ))}
+            </div>
+          </div>
+
+          <h3 style={{ fontSize: '20px', fontWeight: 'bold', textAlign: 'center', marginBottom: '20px' }}>Zarezerwuj wizytę</h3>
+          <div style={{ marginBottom: '20px' }}>
+            <label style={{ fontSize: '16px', fontWeight: 'bold', marginBottom: '5px', display: 'block' }}>Imię pacjenta:</label>
+            <input
+              type="text"
+              onChange={(e) => setPatientName(e.target.value)}
+              value={patientName}
+              style={{ width: '100%', borderRadius: '4px', padding: '10px' }}
+              placeholder="Wpisz imię pacjenta"
+            />
+          </div>
+          <div style={{ marginBottom: '20px' }}>
+            <label style={{ fontSize: '16px', fontWeight: 'bold', marginBottom: '5px', display: 'block' }}>Numer telefonu:</label>
+            <input
+              type="tel"
+              onChange={(e) => setPhoneNumber(e.target.value)}
+              value={phoneNumber}
+              style={{ width: '100%', borderRadius: '4px', padding: '10px' }}
+              placeholder="Wpisz numer telefonu"
+            />
+          </div>
+          <div style={{ marginBottom: '20px' }}>
+            <label style={{ fontSize: '16px', fontWeight: 'bold', marginBottom: '5px', display: 'block' }}>Wybierz datę i godzinę:</label>
+            <input
+              type="datetime-local"
+              onChange={(e) => setSelectedDate(new Date(e.target.value))}
+              value={selectedDate.toISOString().slice(0, 16)} // Formatowanie daty do 'YYYY-MM-DDTHH:MM'
+              style={{ width: '100%', borderRadius: '4px', padding: '10px' }}
+            />
+          </div>
+         
+          <button 
+            onClick={createCalendarEvent} 
+            style={{ width: '100%', padding: '15px', fontSize: '18px', fontWeight: 'bold', backgroundColor: '#28a745', color: '#fff', border: 'none', borderRadius: '4px', cursor: 'pointer', marginBottom: '10px' }}
+            disabled={!selectedService || !selectedDate || !selectedTime}
+          >
+            Zarezerwuj wizytę
+          </button>
+          <button 
+            onClick={signOut}
+            style={{ width: '100%', padding: '15px', fontSize: '18px', fontWeight: 'bold', backgroundColor: '#dc3545', color: '#fff', border: 'none', borderRadius: '4px', cursor: 'pointer' }}
+          >
+            Wyloguj się
+          </button>
+        </>
+      ) : (
+        <button 
+          onClick={googleSignIn} 
+          style={{ width: '100%', padding: '15px', fontSize: '18px', fontWeight: 'bold', backgroundColor: '#007BFF', color: '#fff', border: 'none', borderRadius: '4px', cursor: 'pointer' }}
+        >
+          Zaloguj się przez Google
+        </button>
+      )}
     </div>
   );
 }
