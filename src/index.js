e4cee3bc9899e1ad8172b885ec2e81241a107923
piotr <p@wp.pl>
2024-10-21 20:03:07 +0200

    add env

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import reportWebVitals from './reportWebVitals';
 import { createClient } from '@supabase/supabase-js';
 import { SessionContextProvider } from '@supabase/auth-helpers-react';
 
-const supabaseUrl = 'https://edaolfcwogortdsiqgrh.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVkYW9sZmN3b2dvcnRkc2lxZ3JoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk0OTUwODgsImV4cCI6MjA0NTA3MTA4OH0.yTMJ4Nq2n-35SuvTAIc5XLTKPeRL_084YnsViZj_JRg';
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
